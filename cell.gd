653b52a5bbb44574fcf4152986e5aa39e9f2e1f3
Juan Pablo Villaseca <juan.villaseca@timining.com>
2025-10-17 20:13:30 -0300

    added shader

--- cell.gd
+++ cell.gd
@@ -20,10 +20,19 @@ var column: int = -1
 var iteration_used_as_altruist: int = -1
 
 func _ready() -> void:
+	# Create shader material if it doesn't exist
+	if material == null:
+		var shader_mat = ShaderMaterial.new()
+		shader_mat.shader = preload("res://cell.gdshader")
+		shader_mat.set_shader_parameter("tint_strength", 1.0) # Adjust value between 0.0 and 1.0
+		material = shader_mat
 	_update_color()
 
 func _update_color():
-	modulate = cell_type_to_color(CellType)
+	if material is ShaderMaterial:
+		material.set_shader_parameter("tint_color", cell_type_to_color(CellType))
+	else:
+		self_modulate = cell_type_to_color(CellType)
 		
 
 static func cell_type_to_color(cell_type: AltruistType) -> Color:
